21888f980258db59faae124812c1fe3bdfdd08d7
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-06-20 14:13:20 -0400

    Update and rename 05e_tempRastersv7_rcp8_noEvol.R to 05e_tempRastersv8_rcp8_noEvol.R

--- 05e_tempRastersv8_rcp8_noEvol.R
+++ 05e_tempRastersv8_rcp8_noEvol.R
@@ -14,8 +14,8 @@ t<-ncvar_get(SST,"time")
 Temp.array<-ncvar_get(SST,"sst")
 dim(Temp.array)
 
-tol.lb<-0.9
-tol.ub<-31.9
+tol.lb<-1.8
+tol.ub<-30.1
 errat.lb<-5
 errat.ub<-25
 pref.lb<-13.6
@@ -275,10 +275,10 @@ extent(TOL)<-extent(ERRAT)<-extent(PREF)<-extent(COMBOTE)<-extent(temp.min)
 extent(TOLW)<-extent(ERRATW)<-extent(COMBOTEW)<-extent(temp.min25WARMED)
 
 #Save rasters (use ",overwrite=TRUE" to overwrite files, not in code to avoid accidents)
-writeRaster(TOL,"Processed_files/TOL_Nov3_rcp8_noEvol.asc",format="ascii")
-writeRaster(ERRAT,"Processed_files/ERRAT_Nov3_rcp8_noEvol.asc",format="ascii")
-writeRaster(PREF,"Processed_files/PREF_Nov3_rcp8_noEvol.asc",format="ascii")
-writeRaster(COMBOTE,"Processed_files/COMBOTE_Nov3_rcp8_noEvol.asc",format="ascii")
-writeRaster(TOLW,"Processed_files/TOLW_Nov3_rcp8_noEvol.asc",format="ascii")
-writeRaster(ERRATW,"Processed_files/ERRATW_Nov3_rcp8_noEvol.asc",format="ascii")
-writeRaster(COMBOTEW,"Processed_files/COMBO_TOL_E_W_Nov3_rcp8_noEvol.asc",format="ascii")
+writeRaster(TOL,"Processed_files/TOL_June20_rcp8_noEvol.asc",format="ascii")
+writeRaster(ERRAT,"Processed_files/ERRAT_June20_rcp8_noEvol.asc",format="ascii")
+writeRaster(PREF,"Processed_files/PREF_June20_rcp8_noEvol.asc",format="ascii")
+writeRaster(COMBOTE,"Processed_files/COMBOTE_June20_rcp8_noEvol.asc",format="ascii")
+writeRaster(TOLW,"Processed_files/TOLW_June20_rcp8_noEvol.asc",format="ascii")
+writeRaster(ERRATW,"Processed_files/ERRATW_June20_rcp8_noEvol.asc",format="ascii")
+writeRaster(COMBOTEW,"Processed_files/COMBO_TOL_E_W_June20_rcp8_noEvol.asc",format="ascii")
