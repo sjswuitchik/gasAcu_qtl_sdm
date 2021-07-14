c55ca48239afe1ebf6ed0b08cc779fa5f1928878
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-07-14 12:18:29 -0400

    Update 07_map_area_calcs.R

--- 07_map_area_calcs.R
+++ 07_map_area_calcs.R
@@ -15,24 +15,24 @@ library(viridis)
 
 ###########################################################################################################################
 # Load rasters, add projection
-r4NC<-raster("COMBOTE_Nov26_rcp4_noEvol.asc")
+r4NC<-raster("COMBOTE_June20_rcp4_noEvol.asc")
 proj4string(r4NC)<-CRS("+init=epsg:4326")
-r4NW<-raster("COMBO_TOL_E_W_Nov26_rcp4_noEvol.asc")
+r4NW<-raster("COMBO_TOL_E_W_June20_rcp4_noEvol.asc")
 proj4string(r4NW)<-CRS("+init=epsg:4326")
-r4EW<-raster("COMBO_TOL_E_W_Nov3_rcp4_evol.asc")
+r4EW<-raster("COMBO_TOL_E_W_June20_rcp4_lowerEvol.asc")
 proj4string(r4EW)<-CRS("+init=epsg:4326")
-r8NW<-raster("COMBO_TOL_E_W_Nov3_rcp8_noEvol.asc")
+r8NW<-raster("COMBO_TOL_E_W_June20_rcp8_noEvol.asc")
 proj4string(r8NW)<-CRS("+init=epsg:4326")
-r8EW<-raster("COMBO_TOL_E_W_Nov3_rcp8_evol.asc")
+r8EW<-raster("COMBO_TOL_E_W_June20_rcp8_lowerEvol.asc")
 proj4string(r8EW)<-CRS("+init=epsg:4326")
-r4EWadjPVE<-raster("COMBO_TOL_E_W_Nov30_rcp4_adjPVE_minOnly.asc")
-proj4string(r4EWadjPVE)<-CRS("+init=epsg:4326")
-r8EWadjPVE<-raster("COMBO_TOL_E_W_Nov30_rcp8_adjPVE_minOnly.asc")
-proj4string(r8EWadjPVE)<-CRS("+init=epsg:4326")
-r4EWadjPVEv6<-raster("COMBO_TOL_E_W_Feb9_rcp4_tolerrat_adj.asc")
-proj4string(r4EWadjPVEv6)<-CRS("+init=epsg:4326")
-r8EWadjPVEv6<-raster("COMBO_TOL_E_W_Feb9_rcp8_tolerrat_adj.asc")
-proj4string(r8EWadjPVEv6)<-CRS("+init=epsg:4326") 
+#r4EWadjPVE<-raster("COMBO_TOL_E_W_Nov30_rcp4_adjPVE_minOnly.asc")
+#proj4string(r4EWadjPVE)<-CRS("+init=epsg:4326")
+#r8EWadjPVE<-raster("COMBO_TOL_E_W_Nov30_rcp8_adjPVE_minOnly.asc")
+#proj4string(r8EWadjPVE)<-CRS("+init=epsg:4326")
+#r4EWadjPVEv6<-raster("COMBO_TOL_E_W_Feb9_rcp4_tolerrat_adj.asc")
+#proj4string(r4EWadjPVEv6)<-CRS("+init=epsg:4326")
+#r8EWadjPVEv6<-raster("COMBO_TOL_E_W_Feb9_rcp8_tolerrat_adj.asc")
+#proj4string(r8EWadjPVEv6)<-CRS("+init=epsg:4326") 
 
 # Find the raster values
 unique(r4NC)
@@ -129,61 +129,61 @@ area(poly.r4NW)/area(poly.r4NC)
 #### Comparison 6: RCP 8.5 current vs no evol warmer (r8NW/r4NC) ####
 area(poly.r8NW)/area(poly.r4NC)
 
-#### Comparison 7: RCP 4.5 evol warmer vs adj PVE evol warmer (r4EW/r4EWadjPVE) ####
-
-# process adjPVE rasters
-r4EWadjPVE[r4EWadjPVE!=3]<-NA
-pdf("r4EWadjPVE.pdf")
-plot(r4EWadjPVE)
-dev.off()
-
-r8EWadjPVE[r8EWadjPVE!=3]<-NA
-pdf("r8EWadjPVE.pdf")
-plot(r8EWadjPVE)
-dev.off()
-
-# Change projection to an equal area projection (useable for areas N of 45 lat)
-proj4string(r4EWadjPVE)<-CRS("+init=epsg:3572") 
-proj4string(r8EWadjPVE)<-CRS("+init=epsg:3572")
-
-# Convert to polygon
-poly.r4EWadjPVE<-rasterToPolygons(r4EWadjPVE,na.rm=TRUE,dissolve=TRUE)
-poly.r8EWadjPVE<-rasterToPolygons(r8EWadjPVE,na.rm=TRUE,dissolve=TRUE)
-
-#Difference in the area as a proportion
-area(poly.r4EW)/area(poly.r4EWadjPVE)
-
-#### Comparsion 8: RCP 8.5 evol warmer vs adj PVE evol warmer (r8EW/r8EWadjPVE) ####
-area(poly.r8EW)/area(poly.r8EWadjPVE)
-
-#### Comparison 9: RCP 4.5 current vs adj PVE evol warmer (r4EWadjPVE/r4NC) ####
-# NB: need to run whole script to get the properly extent-matched r4NC here
-area(poly.r4EWadjPVE)/area(poly.r4NC)
-
-#### Comparison 10: RCP 8.5 current vs adj PVE evol warmer (r8EWadjPVE/r4NC) ####
-area(poly.r8EWadjPVE)/area(poly.r4NC)
-
-#### Comparison 11: RCP 4.5 current vs adj PVE evol warmer v6 (CTmin+CTmax evol) (r4EWadjPVEv6/r4NC) ####
-r4EWadjPVEv6[r4EWadjPVEv6!=3]<-NA
-pdf("r4EWadjPVEv6.pdf")
-plot(r4EWadjPVEv6)
-dev.off()
-
-r8EWadjPVEv6[r8EWadjPVEv6!=3]<-NA
-pdf("r8EWadjPVEv6.pdf")
-plot(r8EWadjPVEv6)
-dev.off()
-
-# Change projection to an equal area projection (useable for areas N of 45 lat)
-proj4string(r4EWadjPVEv6)<-CRS("+init=epsg:3572") 
-proj4string(r8EWadjPVEv6)<-CRS("+init=epsg:3572")
-
-# Convert to polygon
-poly.r4EWadjPVEv6<-rasterToPolygons(r4EWadjPVEv6,na.rm=TRUE,dissolve=TRUE)
-poly.r8EWadjPVEv6<-rasterToPolygons(r8EWadjPVEv6,na.rm=TRUE,dissolve=TRUE)
-
-area(poly.r4EWadjPVEv6)/area(poly.r4NC)
-
-#### Comparison 12: RCP 8.5 current vs adj PVE evol warmer v6 (CTmin+CTmax evol) (r8EWadjPVEv6/r4NC) ####
-area(poly.r8EWadjPVEv6)/area(poly.r4NC)
-
+##### Comparison 7: RCP 4.5 evol warmer vs adj PVE evol warmer (r4EW/r4EWadjPVE) ####
+#
+## process adjPVE rasters
+#r4EWadjPVE[r4EWadjPVE!=3]<-NA
+#pdf("r4EWadjPVE.pdf")
+#plot(r4EWadjPVE)
+#dev.off()
+#
+#r8EWadjPVE[r8EWadjPVE!=3]<-NA
+#pdf("r8EWadjPVE.pdf")
+#plot(r8EWadjPVE)
+#dev.off()
+#
+## Change projection to an equal area projection (useable for areas N of 45 lat)
+#proj4string(r4EWadjPVE)<-CRS("+init=epsg:3572") 
+#proj4string(r8EWadjPVE)<-CRS("+init=epsg:3572")
+#
+## Convert to polygon
+#poly.r4EWadjPVE<-rasterToPolygons(r4EWadjPVE,na.rm=TRUE,dissolve=TRUE)
+#poly.r8EWadjPVE<-rasterToPolygons(r8EWadjPVE,na.rm=TRUE,dissolve=TRUE)
+#
+##Difference in the area as a proportion
+#area(poly.r4EW)/area(poly.r4EWadjPVE)
+#
+##### Comparsion 8: RCP 8.5 evol warmer vs adj PVE evol warmer (r8EW/r8EWadjPVE) ####
+#area(poly.r8EW)/area(poly.r8EWadjPVE)
+#
+##### Comparison 9: RCP 4.5 current vs adj PVE evol warmer (r4EWadjPVE/r4NC) ####
+## NB: need to run whole script to get the properly extent-matched r4NC here
+#area(poly.r4EWadjPVE)/area(poly.r4NC)
+#
+##### Comparison 10: RCP 8.5 current vs adj PVE evol warmer (r8EWadjPVE/r4NC) ####
+#area(poly.r8EWadjPVE)/area(poly.r4NC)
+#
+##### Comparison 11: RCP 4.5 current vs adj PVE evol warmer v6 (CTmin+CTmax evol) (r4EWadjPVEv6/r4NC) ####
+#r4EWadjPVEv6[r4EWadjPVEv6!=3]<-NA
+#pdf("r4EWadjPVEv6.pdf")
+#plot(r4EWadjPVEv6)
+#dev.off()
+#
+#r8EWadjPVEv6[r8EWadjPVEv6!=3]<-NA
+#pdf("r8EWadjPVEv6.pdf")
+#plot(r8EWadjPVEv6)
+#dev.off()
+#
+## Change projection to an equal area projection (useable for areas N of 45 lat)
+#proj4string(r4EWadjPVEv6)<-CRS("+init=epsg:3572") 
+#proj4string(r8EWadjPVEv6)<-CRS("+init=epsg:3572")
+#
+## Convert to polygon
+#poly.r4EWadjPVEv6<-rasterToPolygons(r4EWadjPVEv6,na.rm=TRUE,dissolve=TRUE)
+#poly.r8EWadjPVEv6<-rasterToPolygons(r8EWadjPVEv6,na.rm=TRUE,dissolve=TRUE)
+#
+#area(poly.r4EWadjPVEv6)/area(poly.r4NC)
+#
+##### Comparison 12: RCP 8.5 current vs adj PVE evol warmer v6 (CTmin+CTmax evol) (r8EWadjPVEv6/r4NC) ####
+#area(poly.r8EWadjPVEv6)/area(poly.r4NC)
+#
