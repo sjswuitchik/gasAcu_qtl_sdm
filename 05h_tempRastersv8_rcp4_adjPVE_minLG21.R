f0b745ba723271c747526433bf36ca56ff12017f
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-07-14 13:42:02 -0400

    Update 05h_tempRastersv8_rcp4_adjPVE_minLG21.R

--- 05h_tempRastersv8_rcp4_adjPVE_minLG21.R
+++ 05h_tempRastersv8_rcp4_adjPVE_minLG21.R
@@ -26,7 +26,7 @@ pref.ub<-19.9
 #Warmer world - evolution of traits
 evol.tol.lb <- tol.lb - 1.34
 evol.tol.ub <- tol.ub 
-evol.errat.lb <- errat.lb 
+evol.errat.lb <- errat.lb - 1.34
 evol.errat.ub <- errat.ub 
 evol.pref.lb <- pref.lb
 evol.pref.ub <- pref.ub
