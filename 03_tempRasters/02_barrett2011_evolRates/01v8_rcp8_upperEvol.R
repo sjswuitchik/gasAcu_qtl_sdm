2c6e1e9c14632aebec0a866f6609d9143317af9b
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2021-12-28 06:36:03 -0800

    Update 01v8_rcp8_upperEvol.R

--- 03_tempRasters/02_barrett2011_evolRates/01v8_rcp8_upperEvol.R
+++ 03_tempRasters/02_barrett2011_evolRates/01v8_rcp8_upperEvol.R
@@ -22,9 +22,9 @@ pref.lb<-13.6
 pref.ub<-19.9
 
 #Warmer world - evolution of traits
-evol.tol.lb <- tol.lb 
+evol.tol.lb <- tol.lb - 2.5
 evol.tol.ub <- tol.ub + 2.5 
-evol.errat.lb <- errat.lb 
+evol.errat.lb <- errat.lb - 2.5
 evol.errat.ub <- errat.ub + 2.5
 evol.pref.lb <- pref.lb
 evol.pref.ub <- pref.ub
