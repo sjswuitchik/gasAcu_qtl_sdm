61789a0a16f07b039994840a88c0737abf9e3ef1
sjswuitchik <sjswuitchik@g.harvard.edu>
2022-02-02 15:41:09 -0800

    Updating map scripts to include all evol rate scenarios

--- 04_maps/02_barrett2011_evolRates/01_rcp8_lowerEvol.R
+++ 04_maps/02_barrett2011_evolRates/01_rcp8_lowerEvol.R
@@ -15,14 +15,15 @@ library(viridis)
 
 ###########################################################################################################################
 
+#### Barrett rate
 #Load raster of combined tolerance and erratic behaviour
-COMBOTEWARM<-raster("Processed_files/COMBO_TOL_E_W_June20_rcp8_lowerEvol.asc")
+COMBOTEWARM<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerEvol.asc")
 
 #Load bathymetry to get coastlines (nc file)
-Bathy<-nc_open("GEBCO_2014_2D_-179.7777_45.2207_-120.8539_76.3978.nc")
+Bathy<-nc_open("required_files/GEBCO_2014_2D_-179.7777_45.2207_-120.8539_76.3978.nc")
 
 #Load shapefile of bathymetry/sea ice availibility Current World
-suit<-readOGR("Bathy&SeaIce-prefered")
+suit<-readOGR("required_files/Bathy&SeaIce-prefered")
 
 #Find lat and lon of cells
 lon<-ncvar_get(Bathy,"lon")
@@ -56,14 +57,24 @@ A<-raster("SaraUseThis.tif")
 #Dissolve polygons together, to form one polygon of possible habitats
 #new<-unionSpatialPolygons(suit,rep(1,length(suit$Id)))
 
+#### Sanderson
+SAN_COMBOTEWARM<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerEvol_sanderson.asc")
+
 # set up plot parameters
 viridis <- viridis_pal(direction = 1, option = "C")
 cols <- viridisLite::viridis(3)
 ltext<-c("Outside Physiol Limits","Within Physiol Limits","Normal Behav") #legend text
 
-#Plot-Warmer World
+#Plot-Warmer World Barrett
 pdf("Figs/rcp8_lowerEvol_warmer.pdf")
 plot(D, col="grey",axes=F,legend=F)
 plot(COMBOTEWARM, add=T, legend=F, at=c(2,3),col=cols[2:3])
+legend("bottomleft",legend=ltext,fill=cols,bg="white")
+dev.off()
+
+#Plot-Warmer World Sanderson
+pdf("Figs/rcp8_lowerEvol_warmer_sanderson.pdf")
+plot(D, col="grey",axes=F,legend=F)
+plot(SAN_COMBOTEWARM, add=T, legend=F, at=c(2,3),col=cols[2:3])
 legend("bottomleft",legend=ltext,fill=cols,bg="white")
 dev.off()
