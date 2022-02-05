4cbfd961baf8c0e854a65b73de6a4e8bb36e07f5
sjswuitchik <sjswuitchik@g.harvard.edu>
2022-02-04 19:37:52 -0800

    Updated file paths, new rasters, fig outputs for pub

--- 04_maps/02_barrett2011_evolRates/04_map_area_calcs.R
+++ 04_maps/02_barrett2011_evolRates/04_map_area_calcs.R
@@ -13,46 +13,50 @@ library(maptools)
 library(ncdf4)
 library(viridis)
 
-###########################################################################################################################
+############################################################################################################
 # Load rasters, add projection
-r4NC<-raster("COMBOTE_June20_rcp4_noEvol.asc")
+r4NC<-raster("outputs/Processed_files/COMBOTE_jan2022_rcp4_noEvol.asc")
 proj4string(r4NC)<-CRS("+init=epsg:4326")
-r4NW<-raster("COMBO_TOL_E_W_June20_rcp4_noEvol.asc")
+r4NW<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_noEvol.asc")
 proj4string(r4NW)<-CRS("+init=epsg:4326")
-r4EW<-raster("COMBO_TOL_E_W_June20_rcp4_lowerEvol.asc")
+r4EW<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_lowerEvol.asc")
 proj4string(r4EW)<-CRS("+init=epsg:4326")
-r8NW<-raster("COMBO_TOL_E_W_June20_rcp8_noEvol.asc")
+r8NW<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_noEvol.asc")
 proj4string(r8NW)<-CRS("+init=epsg:4326")
-r8EW<-raster("COMBO_TOL_E_W_June20_rcp8_lowerEvol.asc")
+r8EW<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerEvol.asc")
 proj4string(r8EW)<-CRS("+init=epsg:4326")
-r4min3<-raster("COMBO_TOL_E_W_Jul14_rcp4_adjPVE_minLG3.asc")
+r4min3<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_lowerEvol.asc") # rasters are identical
 proj4string(r4min3)<-CRS("+init=epsg:4326")
-r4min21<-raster("COMBO_TOL_E_W_Jul14_rcp4_adjPVE_minLG21.asc")
+r4min21<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_lowerEvol.asc") # rasters are identical
 proj4string(r4min21)<-CRS("+init=epsg:4326")
-r4max21<-raster("COMBO_TOL_E_W_Jul14_rcp4_adjPVE_maxLG21.asc")
-proj4string(r4max21)<-CRS("+init=epsg:4326")
-r4max12<-raster("COMBO_TOL_E_W_Jul14_rcp4_adjPVE_maxLG12.asc")
+r4max20<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_adjPVE_maxLG20.asc")
+proj4string(r4max20)<-CRS("+init=epsg:4326")
+r4max12<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_adjPVE_maxLG12.asc")
 proj4string(r4max12)<-CRS("+init=epsg:4326")
-r8min3<-raster("COMBO_TOL_E_W_Jul14_rcp8_adjPVE_minLG3.asc")
+r8min3<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerEvol.asc") # rasters are identical
 proj4string(r8min3)<-CRS("+init=epsg:4326")
-r8min21<-raster("COMBO_TOL_E_W_Jul14_rcp8_adjPVE_minLG21.asc")
+r8min21<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerEvol.asc") # rasters are identical
 proj4string(r8min21)<-CRS("+init=epsg:4326")
-r8max21<-raster("COMBO_TOL_E_W_Jul14_rcp8_adjPVE_maxLG21.asc")
-proj4string(r8max21)<-CRS("+init=epsg:4326")
-r8max12<-raster("COMBO_TOL_E_W_Jul14_rcp8_adjPVE_maxLG12.asc")
+r8max20<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_adjPVE_maxLG12.asc") # rasters are identical
+proj4string(r8max20)<-CRS("+init=epsg:4326")
+r8max12<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_adjPVE_maxLG12.asc") # rasters are identical 
 proj4string(r8max12)<-CRS("+init=epsg:4326")
+r4max<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_upperEvol.asc")
+proj4string(r4max)<-CRS("+init=epsg:4326")
+r8max<-raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_upperEvol.asc")
+proj4string(r8max)<-CRS("+init=epsg:4326")
 
-## New rasters for Dec 2021 revisions
-r4minMaxEW <- raster("COMBO_TOL_E_W_dec2021_rcp4_lowerUpperEvol.asc")
+## New rasters for Dec revisions
+r4minMaxEW <- raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_lowerUpperEvol.asc")
 proj4string(r4minMaxEW) <- CRS("+init=epsg:4326")
-r8minMaxEW <- raster("COMBO_TOL_E_W_dec2021_rcp8_lowerUpperEvol.asc")
+r8minMaxEW <- raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerUpperEvol.asc")
 proj4string(r8minMaxEW) <- CRS("+init=epsg:4326")
 
 # Find the raster values
 unique(r4NC)
 
 # Verify what the raster values correspond to in this version (should be consistent but check it anyways)
-pdf("raw_r4NC.pdf")
+pdf("outputs/Figs/raw_r4NC.pdf")
 plot(r4NC) 
 dev.off()
 
@@ -63,17 +67,22 @@ dev.off()
 
 # NB: RCP 4.5 current and RCP 8.5 current are identical (because the current day model is not effected by forecasting scenarios) so for ease of coding, only RCP 4.5 current day is used here
 
-#### Comparison 1: RCP 4.5 no evol vs evol in warmer world (r4EW/r4NW) ####
-
+## order of operations
 # Replace the raster components we don't care about with NA (ie/ isolating the 'normal behaviour' envelope)
+# change extents (if necessary)
+# change to equal area projection (useable for areas N of 45 lat)
+# convert to polygon
+# calculate area of polygon
+
+
 # NB: extents are the same between rasters, so don't need to change anything
 r4NW[r4NW!=3]<-NA
-pdf("r4NW.pdf")
+pdf("outputs/Figs/r4NW.pdf")
 plot(r4NW)
 dev.off()
 
 r4EW[r4EW!=3]<-NA
-pdf("r4EW.pdf")
+pdf("outputs/Figs/r4EW.pdf")
 plot(r4EW)
 dev.off()
 
@@ -85,40 +94,34 @@ proj4string(r4EW)<-CRS("+init=epsg:3572")
 poly.r4NW<-rasterToPolygons(r4NW,na.rm=TRUE,dissolve=TRUE)
 poly.r4EW<-rasterToPolygons(r4EW,na.rm=TRUE,dissolve=TRUE)
 
-#Difference in the area as a proportion
-area(poly.r4EW)/area(poly.r4NW)
+# Areas 
+area(poly.r4EW) # 193.9766
+area(poly.r4NW) # 152.5273
 
-#### Comparison 2: RCP 8.5 no evol warmer vs evol warmer (r8EW/r8NW) ####
 # NB: extents are the same
+
 r8NW[r8NW!=3]<-NA
-pdf("r8NW.pdf")
+pdf("outputs/Figs/r8NW.pdf")
 plot(r8NW)
 dev.off()
 
 r8EW[r8EW!=3]<-NA
-pdf("r8EW.pdf")
+pdf("outputs/Figs/r8EW.pdf")
 plot(r8EW)
 dev.off()
 
-# Change projection to an equal area projection (useable for areas N of 45 lat)
 proj4string(r8NW)<-CRS("+init=epsg:3572") 
 proj4string(r8EW)<-CRS("+init=epsg:3572")
 
-# Convert to polygon
 poly.r8NW<-rasterToPolygons(r8NW,na.rm=TRUE,dissolve=TRUE)
 poly.r8EW<-rasterToPolygons(r8EW,na.rm=TRUE,dissolve=TRUE)
 
-# Difference in the area
-area(poly.r8EW)/area(poly.r8NW)
+area(poly.r8EW) # 193.9766
+area(poly.r8NW) # 173.2928
 
-#### Comparison 3: RCP 4.5 current vs evol warmer (r4EW/r4NC) #### 
 
-#Extents need to be identical, make the smaller extent match the larger
-r4NC<-extend(r4NC,r4NW)
-
-#In the smaller raster, replace the raster components we don't care about with NA
 r4NC[r4NC!=3]<-NA
-pdf("r4NC.pdf")
+pdf("outputs/Figs/r4NC.pdf")
 plot(r4NC)
 dev.off()
 
@@ -128,55 +131,43 @@ proj4string(r4NC)<-CRS("+init=epsg:3572")
 # Convert to polygon
 poly.r4NC<-rasterToPolygons(r4NC,na.rm=TRUE,dissolve=TRUE)
 
-#Difference in the area
-area(poly.r4EW)/area(poly.r4NC)
-
-#### Comparison 4: RCP 8.5 current vs evol warmer (r8EW/r4NC) ####
-# NB: all polys have been created for this comparison
-
-# Difference in the area
-area(poly.r8EW)/area(poly.r4NC)
-
-#### Comparison 5: RCP 4.5 current vs no evol warmer (r4NW/r4NC) ####
-area(poly.r4NW)/area(poly.r4NC)
-
-#### Comparison 6: RCP 8.5 current vs no evol warmer (r8NW/r4NC) ####
-area(poly.r8NW)/area(poly.r4NC)
+# Area
+area(poly.r4NC)
 
 #### Areas with new QTL data - July 2021 ####
 r4min3[r4min3!=3]<-NA
-pdf("r4min3.pdf")
+pdf("outputs/Figs/r4min3.pdf")
 plot(r4min3)
 dev.off()
 
 r4min21[r4min21!=3]<-NA
-pdf("r4min21.pdf")
+pdf("outputs/Figs/r4min21.pdf")
 plot(r4min21)
 dev.off()
 
-r4max21[r4max21!=3]<-NA
-pdf("r4max21.pdf")
-plot(r4max21)
+r4max20[r4max20!=3]<-NA
+pdf("outputs/Figs/r4max21.pdf")
+plot(r4max20)
 dev.off()
 
 r4max12[r4max12!=3]<-NA
-pdf("r4max12.pdf")
+pdf("outputs/Figs/r4max12.pdf")
 plot(r4max12)
 dev.off()
 
 r8min3[r8min3!=3]<-NA
-pdf("r8min3.pdf")
+pdf("outputs/Figs/r8min3.pdf")
 plot(r8min3)
 dev.off()
 
 r8min21[r8min21!=3]<-NA
-pdf("r8min21.pdf")
+pdf("outputs/Figs/r8min21.pdf")
 plot(r8min21)
 dev.off()
 
-r8max21[r8max21!=3]<-NA
-pdf("r8max21.pdf")
-plot(r8max21)
+r8max20[r8max20!=3]<-NA
+pdf("outputs/Figs/r8max21.pdf")
+plot(r8max20)
 dev.off()
 
 r8max12[r8max12!=3]<-NA
@@ -187,39 +178,39 @@ dev.off()
 # Change projection to an equal area projection (useable for areas N of 45 lat)
 proj4string(r4min3)<-CRS("+init=epsg:3572") 
 proj4string(r4min21)<-CRS("+init=epsg:3572")
-proj4string(r4max21)<-CRS("+init=epsg:3572")
+proj4string(r4max20)<-CRS("+init=epsg:3572")
 proj4string(r4max12)<-CRS("+init=epsg:3572")
 proj4string(r8min3)<-CRS("+init=epsg:3572") 
 proj4string(r8min21)<-CRS("+init=epsg:3572")
-proj4string(r8max21)<-CRS("+init=epsg:3572")
+proj4string(r8max20)<-CRS("+init=epsg:3572")
 proj4string(r8max12)<-CRS("+init=epsg:3572")
 
 # Convert to polygon
 poly.r4min3<-rasterToPolygons(r4min3,na.rm=TRUE,dissolve=TRUE)
 poly.r4min21<-rasterToPolygons(r4min21,na.rm=TRUE,dissolve=TRUE)
-poly.r4max21<-rasterToPolygons(r4max21,na.rm=TRUE,dissolve=TRUE)
+poly.r4max20<-rasterToPolygons(r4max20,na.rm=TRUE,dissolve=TRUE)
 poly.r4max12<-rasterToPolygons(r4max12,na.rm=TRUE,dissolve=TRUE)
 poly.r8min3<-rasterToPolygons(r8min3,na.rm=TRUE,dissolve=TRUE)
 poly.r8min21<-rasterToPolygons(r8min21,na.rm=TRUE,dissolve=TRUE)
-poly.r8max21<-rasterToPolygons(r8max21,na.rm=TRUE,dissolve=TRUE)
+poly.r8max20<-rasterToPolygons(r8max20,na.rm=TRUE,dissolve=TRUE)
 poly.r8max12<-rasterToPolygons(r8max12,na.rm=TRUE,dissolve=TRUE)
 
 # Area calcs
-area(poly.r4min3)
-area(poly.r4min21)
-area(poly.r4max21)
-area(poly.r4max12)
-area(poly.r8min3)
-area(poly.r8min21)
-area(poly.r8max21)
-area(poly.r8max12)
+area(poly.r4min3) # 193.9766
+area(poly.r4min21) # 193.9766
+area(poly.r4max20) # 152.5273
+area(poly.r4max12) # 152.5273
+area(poly.r8min3) # 193.9766
+area(poly.r8min21) # 193.9766
+area(poly.r8max20) # 173.2928
+area(poly.r8max12) # 173.2928
 
-## New mdoels for Dec 2021 revisions
+## New models for Dec 2021 revisions
 # r4minMaxEW
 # r8minMaxEW
 
 r4minMaxEW[r4minMaxEW!=3]<-NA
-pdf("r4minMaxEW.pdf")
+pdf("outputs/Figs/r4minMaxEW.pdf")
 plot(r4minMaxEW)
 dev.off()
 
@@ -228,7 +219,7 @@ proj4string(r4minMaxEW)<-CRS("+init=epsg:3572")
 poly.r4minMaxEW<-rasterToPolygons(r4minMaxEW,na.rm=TRUE,dissolve=TRUE)
 
 r8minMaxEW[r8minMaxEW!=3]<-NA
-pdf("r8minMaxEW.pdf")
+pdf("outputs/Figs/r8minMaxEW.pdf")
 plot(r8minMaxEW)
 dev.off()
 
@@ -236,8 +227,30 @@ proj4string(r8minMaxEW)<-CRS("+init=epsg:3572")
 
 poly.r8minMaxEW<-rasterToPolygons(r8minMaxEW,na.rm=TRUE,dissolve=TRUE)
 
-area(poly.r4minMaxEW)
-area(poly.r8minMaxEW)
+area(poly.r4minMaxEW) # 193.9766
+area(poly.r8minMaxEW) # 193.9766
 
+## New rasters for Jan revisions
+r4maxW <- raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp4_lowerUpperEvol.asc")
+proj4string(r4maxW) <- CRS("+init=epsg:4326")
+r8maxW <- raster("outputs/Processed_files/COMBO_TOL_E_W_jan2022_rcp8_lowerUpperEvol.asc")
+proj4string(r8maxW) <- CRS("+init=epsg:4326")
 
+r4maxW[r4maxW!=3]<-NA
+r8maxW[r8maxW!=3]<-NA
 
+pdf("outputs/Figs/r4maxW.pdf")
+plot(r4maxW)
+dev.off()
+pdf("outputs/Figs/r8maxW.pdf")
+plot(r8maxW)
+dev.off()
+
+proj4string(r4maxW)<-CRS("+init=epsg:3572") 
+proj4string(r8maxW)<-CRS("+init=epsg:3572") 
+
+poly.r4maxW<-rasterToPolygons(r4maxW,na.rm=TRUE,dissolve=TRUE)
+poly.r8maxW<-rasterToPolygons(r8maxW,na.rm=TRUE,dissolve=TRUE)
+
+area(poly.r4maxW) # 193.9766
+area(poly.r8maxW) # 193.9766
